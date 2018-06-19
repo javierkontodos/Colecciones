7eaf021bae66412d080c158f80e3d282a77018ac
javie <javie@KAPTOP>
2018-06-19 10:26:07 +0200

    Segundo commit

--- Colecciones/src/Main.java
+++ Colecciones/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Set;
 public class Main {
 
 	public static void main(String[] args) {
+		//Crea una coleccion de tipo Set, los repetidos no se guardaran
 		Set coleccionSet = new HashSet();
 		
 		coleccionSet.add("Piratas del Caribe");
@@ -15,6 +16,8 @@ public class Main {
 		coleccionSet.add("Los Vengadores");
 		
 		for (Object elemento : coleccionSet) {
+			//if(elemento instanceof String)
+				//String texto = (String) elemento;
 			System.out.println(elemento);
 		}
 	}
